1ef8c8029bd54e50ff13967a9bd06efc85610233
elecweb <elecwebmaker@gmail.com>
2022-01-19 01:59:32 +0700

    feat: prevent scroll

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,92 +1,103 @@
-import './main.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { convertDate } from '../../lib/time.js';
-import DatePicker from '../../lib/index';
+import "./main.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { convertDate } from "../../lib/time.js";
+import DatePicker from "../../lib/index";
 
 (function main() {
-    class App extends React.Component {
-        state = {
-            time: new Date(),
-            isOpen: false,
-            theme: 'default',
-        }
+  class App extends React.Component {
+    state = {
+      time: new Date(),
+      isOpen: false,
+      theme: "default",
+    };
 
-        handleToggle = (isOpen) => () => {
-            this.setState({ isOpen });
-        }
+    handleToggle = (isOpen) => () => {
+      this.setState({ isOpen });
+    };
 
-        handleThemeToggle = (theme) => () => {
-            this.setState({ theme, isOpen: true });
-        }
+    handleThemeToggle = (theme) => () => {
+      this.setState({ theme, isOpen: true });
+    };
 
-        handleSelect = (time) => {
-            this.setState({ time, isOpen: false });
-        }
+    handleSelect = (time) => {
+      this.setState({ time, isOpen: false });
+    };
 
-        render() {
-            return (
-                <div className="App">
-                    <p className="select-time ">
-                        {convertDate(this.state.time, 'YYYY-MM-DD')}
-                    </p>
-                    <div>
-                        <a
-                            className="select-btn sm"
-                            onClick={this.handleThemeToggle('default')}>
-                            default
-                        </a>
-                        <a
-                            className="select-btn sm"
-                            onClick={this.handleThemeToggle('dark')}>
-                            dark
-                        </a>
-                        <a
-                            className="select-btn sm"
-                            onClick={this.handleThemeToggle('ios')}>
-                            ios
-                        </a>
-                        <a
-                            className="select-btn sm"
-                            onClick={this.handleThemeToggle('android')}>
-                            android
-                        </a>
-                        <a
-                            className="select-btn sm"
-                            onClick={this.handleThemeToggle('android-dark')}>
-                            android-dark
-                        </a>
-                    </div>
-                    <DatePicker
-                        value={this.state.time}
-                        max={new Date()}
-                        theme={this.state.theme}
-                        isOpen={this.state.isOpen}
-                        showCaption
-                        dateConfig={{
-                            'year': {
-                                format: 'YYYY',
-                                caption: '年',
-                                step: 1,
-                            },
-                            'month': {
-                                format: 'M',
-                                caption: '月',
-                                step: 1,
-                            },
-                            'date': {
-                                format: 'D',
-                                caption: '日',
-                                step: 1,
-                            },
-                        }}
-                        onSelect={this.handleSelect}
-                        onCancel={this.handleToggle(false)} />
-                </div>
-            );
-        }
+    render() {
+      return (
+        <div
+          className="App"
+          style={{
+            height: 4000,
+          }}
+        >
+          <p className="select-time ">
+            {convertDate(this.state.time, "YYYY-MM-DD")}
+          </p>
+          <div>
+            <a
+              className="select-btn sm"
+              onClick={this.handleThemeToggle("default")}
+            >
+              default
+            </a>
+            <a
+              className="select-btn sm"
+              onClick={this.handleThemeToggle("dark")}
+            >
+              dark
+            </a>
+            <a
+              className="select-btn sm"
+              onClick={this.handleThemeToggle("ios")}
+            >
+              ios
+            </a>
+            <a
+              className="select-btn sm"
+              onClick={this.handleThemeToggle("android")}
+            >
+              android
+            </a>
+            <a
+              className="select-btn sm"
+              onClick={this.handleThemeToggle("android-dark")}
+            >
+              android-dark
+            </a>
+          </div>
+          <DatePicker
+            value={this.state.time}
+            max={new Date()}
+            theme={this.state.theme}
+            isOpen={this.state.isOpen}
+            isPopup={false}
+            showCaption
+            dateConfig={{
+              year: {
+                format: "YYYY",
+                caption: "年",
+                step: 1,
+              },
+              month: {
+                format: "M",
+                caption: "月",
+                step: 1,
+              },
+              date: {
+                format: "D",
+                caption: "日",
+                step: 1,
+              },
+            }}
+            onSelect={this.handleSelect}
+            onCancel={this.handleToggle(false)}
+          />
+        </div>
+      );
     }
+  }
 
-
-    ReactDOM.render(<App />, document.getElementById('react-box'));
-}());
+  ReactDOM.render(<App />, document.getElementById("react-box"));
+})();
